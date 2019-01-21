753e988522d369db349089105929b749750bd1d9
Marco Walz <walz.marcoo@gmail.com>
2019-01-22 00:37:59 +0100

    test generation and recovery of keystore. libsodium must be installed on the operating system

--- src/main/java/com/kryptokrauts/aeternity/sdk/constants/Argon2Configuration.java
+++ src/main/java/com/kryptokrauts/aeternity/sdk/constants/Argon2Configuration.java
@@ -1,28 +1,22 @@
 package com.kryptokrauts.aeternity.sdk.constants;
 
-import org.bouncycastle.crypto.params.Argon2Parameters;
-
 public interface Argon2Configuration
 {
 
-    public static final String SECRET_TYPE = "ed25519";
+    String SECRET_TYPE = "ed25519";
 
-    public static final String SYMMETRIC_ALGORITHM = "xsalsa20-poly1305";
+    String SYMMETRIC_ALGORITHM = "xsalsa20-poly1305";
 
-    public static final int MEMLIMIT_KIB = 65536;
+    int MEMLIMIT_KIB = 65536;
 
-    public static final int OPSLIMIT = 2;
+    int OPSLIMIT = 2;
 
-    public static final int PARALLELISM = 1;
+    int PARALLELISM = 1;
 
-    public static final int VERSION = 1;
+    int VERSION = 1;
 
-    public static final int argon2Parameter = Argon2Parameters.ARGON2_id;
+    int DEFAULT_SALT_LENGTH = 16;
 
-    public static final int SALT_HEX_SIZE = 16;
-
-    public static final int NONCE_HEX_SIZE = 24;
-
-    public static final String argon2Mode = "argon2id";
+    String argon2Mode = "argon2id";
 
 }
