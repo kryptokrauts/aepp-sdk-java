d279bfc2a04db2fedb8df241bc330dfc48b8f749
Marco Walz <walz.marcoo@gmail.com>
2019-01-23 03:45:31 +0100

    docker setup for integrationTest. spendTx still failing

--- src/integrationTest/java/com/kryptokrauts/aeternity/generated/epoch/api/TransactionApiTest.java
+++ src/integrationTest/java/com/kryptokrauts/aeternity/generated/epoch/api/TransactionApiTest.java
@@ -1,6 +1,8 @@
 package com.kryptokrauts.aeternity.generated.epoch.api;
 
+import com.kryptokrauts.aeternity.generated.epoch.model.PostTxResponse;
 import com.kryptokrauts.aeternity.generated.epoch.model.SpendTx;
+import com.kryptokrauts.aeternity.generated.epoch.model.Tx;
 import com.kryptokrauts.aeternity.generated.epoch.model.UnsignedTx;
 import com.kryptokrauts.aeternity.sdk.config.AEConfig;
 import com.kryptokrauts.aeternity.sdk.domain.secret.impl.BaseKeyPair;
@@ -8,6 +10,7 @@ import io.reactivex.Observable;
 import io.vertx.ext.unit.Async;
 import io.vertx.ext.unit.TestContext;
 import org.bouncycastle.crypto.CryptoException;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 
@@ -15,8 +18,6 @@ import java.math.BigInteger;
 import java.util.concurrent.ExecutionException;
 
 public class TransactionApiTest extends BaseTest {
-
-    private static final String recipient = "ak_21C5mC3RWkS4sTpaNjZapXpY3jVWZZj11cE8MsUfzSqyeguzBb";
 
     @Test
     public void buildNativeTransactionTest(TestContext context) throws ExecutionException, InterruptedException {
@@ -46,7 +47,7 @@ public class TransactionApiTest extends BaseTest {
                 });
     }
 
-    //@Ignore // currently invalidTx
+    //@Ignore // tx is invalid at the moment
     @Test
     public void postSpendTxTest(TestContext context) throws ExecutionException, InterruptedException, CryptoException {
         Async async = context.async();
@@ -55,32 +56,26 @@ public class TransactionApiTest extends BaseTest {
         SpendTx spendTx = new SpendTx();
         spendTx.setSenderId(keyPair.getPublicKey());
         System.out.println(keyPair.getPublicKey());
-        spendTx.setRecipientId(recipient);
+
+        BaseKeyPair kp = keyPairService.generateBaseKeyPair();
+        spendTx.setRecipientId(kp.getPublicKey());
+
         spendTx.setAmount(BigInteger.valueOf(1));
         spendTx.setPayload("payload");
         spendTx.setFee(BigInteger.valueOf(AEConfig.DEFAULT_FEE));
-        spendTx.setTtl(BigInteger.valueOf(AEConfig.DEFAULT_TX_TTL));
+        spendTx.setTtl(BigInteger.valueOf(20000));
         spendTx.setNonce(BigInteger.valueOf(1));
-//        AccountApi accountApi = new AccountApi(new AccountApiImpl());
-//        accountApi.rxGetAccountByPubkey(keyPair.getPublicKey()).toObservable().subscribe(it -> {
-//                    System.out.println(it.getNonce());
-//                    Assertions.assertEquals(it.getNonce().longValue(), 0);
-//                    async.complete();
-//                },
-//                failure -> {
-//                    context.fail();
-//                });
 
-//        UnsignedTx unsignedTxNative = transactionServiceNative.createTx(spendTx).toFuture().get();
-//        Tx signedTx = transactionServiceNative.signTransaction(unsignedTxNative, keyPair.getPrivateKey());
-//        Observable<PostTxResponse> txResponseObservable = transactionServiceNative.postTransaction(signedTx);
-//        txResponseObservable.subscribe(
-//                it -> {
-//                    Assertions.assertEquals(it.getTxHash(), transactionServiceNative.computeTxHash(signedTx.getTx()));
-//                    async.complete();
-//                },
-//                failure -> {
-//                    context.fail();
-//                });
+        UnsignedTx unsignedTxNative = transactionServiceNative.createTx(spendTx).toFuture().get();
+        Tx signedTx = transactionServiceNative.signTransaction(unsignedTxNative, keyPair.getPrivateKey());
+        Observable<PostTxResponse> txResponseObservable = transactionServiceNative.postTransaction(signedTx);
+        txResponseObservable.subscribe(
+                it -> {
+                    Assertions.assertEquals(it.getTxHash(), transactionServiceNative.computeTxHash(signedTx.getTx()));
+                    async.complete();
+                },
+                failure -> {
+                    context.fail();
+                });
     }
 }
