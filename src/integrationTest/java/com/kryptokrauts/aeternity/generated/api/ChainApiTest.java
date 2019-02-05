f11c984a9814e134cded4226e4ba1a00cdf1ab7a
Marco Walz <walz.marcoo@gmail.com>
2019-02-05 23:42:28 +0100

    return Single instead of Observable

--- src/integrationTest/java/com/kryptokrauts/aeternity/generated/api/ChainApiTest.java
+++ src/integrationTest/java/com/kryptokrauts/aeternity/generated/api/ChainApiTest.java
@@ -1,28 +1,26 @@
 package com.kryptokrauts.aeternity.generated.api;
 
+import com.kryptokrauts.aeternity.generated.model.KeyBlock;
+import com.kryptokrauts.aeternity.sdk.service.chain.ChainServiceFactory;
+import io.reactivex.Single;
+import io.vertx.ext.unit.Async;
+import io.vertx.ext.unit.TestContext;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
-
-import com.kryptokrauts.aeternity.generated.model.KeyBlock;
-import com.kryptokrauts.aeternity.sdk.service.chain.ChainServiceFactory;
-
-import io.reactivex.Observable;
-import io.vertx.ext.unit.Async;
-import io.vertx.ext.unit.TestContext;
 
 public class ChainApiTest extends BaseTest {
 
     @Test
     public void getCurrentKeyBlockTest( TestContext context ) {
         Async async = context.async();
-        Observable<KeyBlock> keyBlockObservable = chainService.getCurrentKeyBlock();
+        Single<KeyBlock> keyBlockObservable = chainService.getCurrentKeyBlock();
         keyBlockObservable.subscribe( keyBlock -> {
             System.out.println( keyBlock.toString() );
             Assertions.assertTrue( keyBlock.getHeight().longValue() > 0 );
             async.complete();
-        }, failure -> {
-            failure.printStackTrace();
+        }, throwable -> {
+            throwable.printStackTrace();
             context.fail();
         } );
     }
