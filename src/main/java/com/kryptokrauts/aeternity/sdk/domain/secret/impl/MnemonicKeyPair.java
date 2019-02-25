5c099635ecd79c1ce84e5174282359d3420b1a51
unknown <dna.fan@gmx.de>
2019-02-25 18:15:06 +0100

    added mnemonic keypair to allow recovering of private keys using password salted seed words compliant to BIP32/39

---
+++ src/main/java/com/kryptokrauts/aeternity/sdk/domain/secret/impl/MnemonicKeyPair.java
@@ -0,0 +1,16 @@
+package com.kryptokrauts.aeternity.sdk.domain.secret.impl;
+
+import java.util.List;
+
+import lombok.Data;
+
+@Data
+public class MnemonicKeyPair extends RawKeyPair{
+	
+	private List<String> mnemonicSeedWords;
+	
+	public MnemonicKeyPair(RawKeyPair rawKeyPair, List<String> mnemonicSeedWords) {		
+		super(rawKeyPair.getPublicKey(),rawKeyPair.getPrivateKey());
+		this.mnemonicSeedWords = mnemonicSeedWords;
+	}
+}
