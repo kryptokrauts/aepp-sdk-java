6eb41d18a4bbfbed53b61da87ac2521cf7dc35c0
Marco Walz <walz.marcoo@gmail.com>
2019-06-12 19:29:52 +0200

    added ContractCallFeeCalculationModel

---
+++ src/main/java/com/kryptokrauts/aeternity/sdk/service/transaction/fee/impl/ContractCallFeeCalculationModel.java
@@ -0,0 +1,20 @@
+package com.kryptokrauts.aeternity.sdk.service.transaction.fee.impl;
+
+import com.kryptokrauts.aeternity.sdk.constants.BaseConstants;
+import com.kryptokrauts.aeternity.sdk.service.transaction.fee.FeeCalculationModel;
+import java.math.BigInteger;
+
+public class ContractCallFeeCalculationModel implements FeeCalculationModel {
+
+  /**
+   * the fee is calculated according to the following formula
+   *
+   * <p>(BASE_GAS * 30 + (byte_size * GAS_PER_BYTE)) * MINIMAL_GAS_PRICE
+   */
+  @Override
+  public BigInteger calculateFee(int tx_byte_size, long minimalGasPrice) {
+    return BigInteger.valueOf(
+        (30 * BaseConstants.BASE_GAS + tx_byte_size * BaseConstants.GAS_PER_BYTE)
+            * minimalGasPrice);
+  }
+}
