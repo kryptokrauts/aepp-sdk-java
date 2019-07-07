38d10978218d5776e1a3999323e0d161e60e0b61
Michel Meier <dna.fan@gmx.de>
2019-07-07 02:28:02 +0200

    Code format

--- src/test/java/com/kryptokrauts/aeternity/sdk/BaseTest.java
+++ src/test/java/com/kryptokrauts/aeternity/sdk/BaseTest.java
@@ -1,21 +1,19 @@
 package com.kryptokrauts.aeternity.sdk;
 
-import java.math.BigInteger;
-
-import org.junit.runner.RunWith;
-
 import com.greghaskins.spectrum.Spectrum;
+import java.math.BigInteger;
+import org.junit.runner.RunWith;
 
 @RunWith(Spectrum.class)
 public class BaseTest {
 
-	public static final String defaultPassword = "kryptokrauts";
+  public static final String defaultPassword = "kryptokrauts";
 
-	public static final String DOMAIN = "kryptokrauts";
+  public static final String DOMAIN = "kryptokrauts";
 
-	public static final String NS = "test";
+  public static final String NS = "test";
 
-	public static final String KK_NAMESPACE = DOMAIN + "." + NS;
+  public static final String KK_NAMESPACE = DOMAIN + "." + NS;
 
-	public static final BigInteger TEST_SALT = new BigInteger("2654988072698203");
+  public static final BigInteger TEST_SALT = new BigInteger("2654988072698203");
 }
