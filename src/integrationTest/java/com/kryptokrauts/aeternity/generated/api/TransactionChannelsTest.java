c93f532b192cc84b96dc2b1f6003f459f36f3d02
Marco Walz <walz.marcoo@gmail.com>
2019-07-19 21:30:25 +0200

    fix broken test

--- src/integrationTest/java/com/kryptokrauts/aeternity/generated/api/TransactionChannelsTest.java
+++ src/integrationTest/java/com/kryptokrauts/aeternity/generated/api/TransactionChannelsTest.java
@@ -4,6 +4,8 @@ import com.kryptokrauts.aeternity.sdk.domain.secret.impl.BaseKeyPair;
 import com.kryptokrauts.aeternity.sdk.service.transaction.type.impl.ChannelCreateTransaction;
 import com.kryptokrauts.aeternity.sdk.util.UnitConversionUtil;
 import com.kryptokrauts.aeternity.sdk.util.UnitConversionUtil.Unit;
+import io.vertx.ext.unit.Async;
+import io.vertx.ext.unit.TestContext;
 import java.math.BigInteger;
 import org.junit.Before;
 import org.junit.FixMethodOrder;
@@ -20,35 +22,60 @@ public class TransactionChannelsTest extends BaseTest {
   public void initBeforeTest() {
     initiator = keyPairService.generateBaseKeyPairFromSecret(TestConstants.BENEFICIARY_PRIVATE_KEY);
     responder = keyPairService.generateBaseKeyPair();
-    BigInteger amount = UnitConversionUtil.toAettos("10", Unit.AE).toBigInteger();
-    BigInteger nonce = getAccount(initiator.getPublicKey()).getNonce();
-    transactionServiceNative
-        .getTransactionFactory()
-        .createSpendTransaction(
-            initiator.getPublicKey(),
-            responder.getPublicKey(),
-            amount,
-            null,
-            BigInteger.ZERO,
-            nonce);
   }
 
   @Test
-  public void channelCreateTest() {
-    BigInteger nonce = getAccount(initiator.getPublicKey()).getNonce();
-    ChannelCreateTransaction channelCreateTransaction =
-        transactionServiceNative
-            .getTransactionFactory()
-            .createChannelCreateTransaction(
-                initiator.getPublicKey(),
-                BigInteger.ZERO,
-                responder.getPublicKey(),
-                BigInteger.ZERO,
-                BigInteger.ZERO,
-                BigInteger.ZERO,
-                BigInteger.ZERO,
-                null,
-                nonce);
+  public void aFundResponderAccount(TestContext context) {
+    Async async = context.async();
+    rule.vertx()
+        .executeBlocking(
+            future -> {
+              try {
+                BigInteger amount = UnitConversionUtil.toAettos("10", Unit.AE).toBigInteger();
+                BigInteger nonce = getAccount(initiator.getPublicKey()).getNonce();
+                transactionServiceNative
+                    .getTransactionFactory()
+                    .createSpendTransaction(
+                        initiator.getPublicKey(),
+                        responder.getPublicKey(),
+                        amount,
+                        "",
+                        BigInteger.ZERO,
+                        nonce);
+              } catch (Throwable e) {
+                context.fail(e);
+              }
+              future.complete();
+            },
+            success -> async.complete());
   }
 
+  @Test
+  public void channelCreateTest(TestContext context) {
+    Async async = context.async();
+    rule.vertx()
+        .executeBlocking(
+            future -> {
+              try {
+                BigInteger nonce = getAccount(initiator.getPublicKey()).getNonce();
+                ChannelCreateTransaction channelCreateTransaction =
+                    transactionServiceNative
+                        .getTransactionFactory()
+                        .createChannelCreateTransaction(
+                            initiator.getPublicKey(),
+                            BigInteger.ZERO,
+                            responder.getPublicKey(),
+                            BigInteger.ZERO,
+                            BigInteger.ZERO,
+                            BigInteger.ZERO,
+                            BigInteger.ZERO,
+                            "",
+                            nonce);
+              } catch (Throwable e) {
+                context.fail(e);
+              }
+              future.complete();
+            },
+            success -> async.complete());
+  }
 }
